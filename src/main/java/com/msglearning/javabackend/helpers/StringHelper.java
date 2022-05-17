97ec7974d77dcfed364a8a6856057dbf248bc91e
Raul Pop <popraul17@gmail.com>
2022-05-17 12:33:34 +0300

    Property Converter

---
+++ src/main/java/com/msglearning/javabackend/helpers/StringHelper.java
@@ -0,0 +1,8 @@
+package com.msglearning.javabackend.helpers;
+
+public class StringHelper {
+
+    public static boolean isNullOrBlank(String param) {
+        return param == null || param.trim().length() == 0;
+    }
+}
--- src/main/java/com/msglearning/javabackend/services/PropertyService.java
+++ src/main/java/com/msglearning/javabackend/services/PropertyService.java
@@ -1,15 +1,47 @@
 package com.msglearning.javabackend.services;
 
 import com.msglearning.javabackend.entity.Property;
+import com.msglearning.javabackend.helpers.StringHelper;
 import com.msglearning.javabackend.repositories.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 public class PropertyService {
 
     @Autowired
     PropertyRepository propertyRepository;
+
+    public Property saveProperty (Property property) {
+            if(StringHelper.isNullOrBlank(property.getName())){
+                System.out.println("Invalid Property");
+                return null;
+            }
+
+        if(StringHelper.isNullOrBlank(property.getAddress())){
+            System.out.println("Invalid address");
+            return null;
+        }
+
+        if (property.getPeople_capacity() == 0){
+            System.out.println("Invalid people capacity");
+            return null;
+        }
+
+        if(property.getRooms() == 0){
+            System.out.println("Invalid Room");
+            return null;
+        }
+
+        if(property.getMp3() == 0){
+            System.out.println("Invalid Area");
+            return null;
+        }
+
+        return propertyRepository.save(property);
+    }
+
 
 
 

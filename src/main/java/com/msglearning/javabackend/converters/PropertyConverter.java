a48c67f5aad3228ad5c838093695ed89fdc94a29
Raul Pop <popraul17@gmail.com>
2022-05-17 10:47:18 +0300

    Property Converter

---
+++ src/main/java/com/msglearning/javabackend/converters/PropertyConverter.java
@@ -0,0 +1,18 @@
+package com.msglearning.javabackend.converters;
+
+import com.msglearning.javabackend.entity.Property;
+import com.msglearning.javabackend.to.PropertyTO;
+
+public class PropertyConverter {
+    public static final PropertyTO convertToTo ( Property property) {
+            return new PropertyTO(property.getId(), property.getName(), property.getAddress(),
+                    property.getMp3(), property.getRooms(), property.getPeople_capacity(), property.getDescription());
+    }
+
+    public static final Property convertToTo ( PropertyTO propertyTO) {
+        return new Property(propertyTO.getId(), propertyTO.getName(), propertyTO.getAddress(),
+                propertyTO.getMp3(), propertyTO.getRooms(), propertyTO.getPeople_capacity(), propertyTO.getDescription());
+    }
+
+
+}
